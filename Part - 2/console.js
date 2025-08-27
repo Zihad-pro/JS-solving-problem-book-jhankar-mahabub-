4d19c0a1b55e4678bcfdb5254ebb560381eea37e
Zihad-pro <zubaedhasan46@gmail.com>
2025-08-27 23:44:16 +0600

    string add

---
+++ Part - 2/console.js
@@ -0,0 +1,27 @@
+// 1 : 
+const age =20;
+console.log(age);
+
+// 2 :
+const booksToRead=10;
+console.log(booksToRead);
+
+// 3
+let wakeUpTime=4;
+console.log(wakeUpTime);
+
+// 4
+const wastedTime=3;
+console.log(wastedTime);
+
+// 5 
+const borrow=800000;
+console.log(borrow);
+
+// 6
+const goals=7;
+console.log(goals);
+
+// 7
+const pillow=3;
+console.log(pillow);
---
+++ Part - 2/string.js
@@ -0,0 +1,28 @@
+// 1
+const name = "zihad";
+console.log(name);
+
+// 2
+const address = "pabna";
+console.log(address);
+
+// 3
+const myBestFriend = "ishardi govt collage";
+console.log(myBestFriend);
+
+// 4
+const fan = "bala";
+console.log(fan);
+
+// 5
+
+const movieOfTheYear = "artugul gazi";
+console.log(movieOfTheYear);
+
+// 6
+const hobby = "kulafiha rashadin jug";
+console.log(hobby);
+
+// 7
+const color = "white";
+console.log(color);
