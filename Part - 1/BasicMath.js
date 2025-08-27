bd4afbb85082f2c239d49e1146f6faaf9497e149
Zihad-pro <zubaedhasan46@gmail.com>
2025-08-27 16:52:39 +0600

    first commit

---
+++ Part - 1/BasicMath.js
@@ -0,0 +1,28 @@
+/*  
+ 1/positive : 12, 3.5
+   Negative : -5 ,-8 
+
+ 2/integer : 23 ,56, 101 ,42 ,89
+   even : 56 ,42 
+   odd - 23 ,19.49, 101 , 89 ,11.37 
+ 
+  3/decimal : 3.4 , 2.75
+  fraction : 7/8 ,1/3
+
+  4/ total sum = 90
+
+  5/total = 36 taka
+  
+  6/total = 1750 taka
+
+  7/ per 56 taka , me total  58 taka
+
+  8/ 15>12 true 
+  4<10 true 
+  7>9 false
+  
+  9/ 97.75
+
+  10/153.86
+
+, */
---
+++ Part - 2/variable.js
@@ -0,0 +1,8 @@
+const dateOfBirth=2004;
+// console.log(DateOfBirth);
+const myBrotherAndSister=0;
+const student=200;
+const marks = 80;
+const rooms=2;
+const dailyStudy=6;
+const total=64;
