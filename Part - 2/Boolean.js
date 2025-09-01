c6454e230068a57d40dfc5159f960ac6961bba4a
Zihad-pro <zubaedhasan46@gmail.com>
2025-09-01 13:01:13 +0600

    boolean  completed

---
+++ Part - 2/Boolean.js
@@ -0,0 +1,26 @@
+// 1
+const myFriendResult = true;
+console.log(myFriendResult);
+
+// 2
+const myCrush = true;
+console.log(myCrush);
+
+// 3
+const celebrating = true;
+console.log(celebrating);
+
+// 4
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+// 5
+const isStudyDone = false;
+
+// 6
+const isPhoneCharged = true;
+
+// 7
+const isDinnerDone = false;
