321f5598d9e99b1e0517017e0935481411ed658a
Zihad-pro <zubaedhasan46@gmail.com>
2025-09-04 22:11:10 +0600

    parse add

---
+++ Part - 3/Parse.js
@@ -0,0 +1,37 @@
+// 1
+const a = "20";
+const b = 10;
+const total = parseInt(a) + b;
+console.log(total);
+
+// 2
+const r = parseInt(3.14159);
+console.log(r);
+
+// 3
+const name = parseInt("premikBoy");
+console.log(name);
+// NAN
+
+// 4
+const x = 3.456;
+const y = 2.789;
+const sum = parseFloat(x + y);
+console.log(sum.toFixed(2));
+
+// 5
+const p = parseInt("56.78");
+const q = parseInt("12.34");
+const totalNumber = p + q;
+console.log(totalNumber);
+
+// 6
+const m = parseInt("10.5678");
+const n = parseInt("5.4321");
+const totalSum = m + n;
+console.log(totalSum.toFixed(1));
+
+// 7
+const o=42.45689754;
+const round=o.toFixed(3);
+console.log(round);
