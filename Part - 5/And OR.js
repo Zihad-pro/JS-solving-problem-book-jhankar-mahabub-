397fb384917224fce915d0959f078acef041cc8a
Zihad-pro <zubaedhasan46@gmail.com>
2025-09-06 13:10:51 +0600

    and or add

---
+++ Part - 5/And OR.js
@@ -0,0 +1,38 @@
+// 1
+const age = 20;
+const height = 61;
+
+if (age > 18 && height > 60) {
+  console.log("gari thalte hobe");
+} else {
+  console.log("gari te bosa tak");
+}
+
+// 2
+const math = 80;
+const english = 95;
+if (math >= 80 || english >= 85) {
+  console.log("your chance conform");
+} else {
+  console.log("toke bewa dewa dewa hobe");
+}
+
+// 3
+const gpa = 5;
+const income = 5000;
+if (gpa == 5 && income <= 10000) {
+  console.log("your scholarship done");
+} else {
+  console.log("baton dew porta hobe");
+}
+
+// 4
+const ageCarir = 50;
+const experience = 5;
+if (ageCarir <= 30 && experience >= 2) {
+  console.log("intervew dita parbe");
+} else {
+  console.log("not interview");
+}
+
+// 5
---
+++ Part - 5/if-else.js
@@ -0,0 +1,47 @@
+// 1
+const goHomeTime = 8;
+if (goHomeTime < 6) {
+  console.log("nasta kawabe");
+} else {
+  console.log("charu khabe");
+}
+
+// 2
+const login = "successfully";
+if (login == "successfully") {
+  console.log("welcome");
+} else {
+  console.log("Get Lost");
+}
+
+// 3
+const run = 5;
+if (run >= 5) {
+  console.log("chocolate");
+} else {
+  console.log("You are fat");
+}
+
+// 4
+const examNumber = 70;
+if (examNumber > 80) {
+  console.log("bike");
+} else {
+  console.log("no bike");
+}
+
+// 5
+const moiveShow = 10;
+if (moiveShow > 9) {
+  console.log("show artulgazi moive");
+} else {
+  console.log("nake teal dewa sowee tak");
+}
+
+// 6
+const temperature = 50;
+if (temperature >= 30) {
+  console.log("ac chalo kor");
+} else {
+  console.log("kombol more dewa sowa tak");
+}
